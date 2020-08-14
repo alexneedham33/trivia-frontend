6abfa9409a45054249d7c93363506e3fc38848c0
Alexander Needham <alexanderneedham@alexandersair2.home>
2020-08-14 16:28:52 +0100

    fix api url

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,9 +1,12 @@
 import axios from "axios";
-import {degNumIndex} from '../components/Wheel/Wheel.js'
+import { degNumIndex } from "../components/Wheel/Wheel.js";
 
 console.log(degNumIndex);
 
 export const getData = () => {
-  const responseEnglish = axios.get(`${process.env.REACT_APP_API_URL}/${degNumIndex}`);
+  const responseEnglish = axios.get(
+    `${process.env.REACT_APP_API_URL}/${degNumIndex}`
+  );
+  console.log("api url", process.env.REACT_APP_API_URL);
   return responseEnglish;
 };
